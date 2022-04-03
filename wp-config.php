73b80d45046817e2ba9b76fb243a476537430415
Utshav Timsina <utshavtimsina123@gmail.com>
2022-04-03 21:49:21 +0545

    Docker Network issue fix

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', ' ' );
 define( 'DB_PASSWORD', ' ' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '172.20.0.2' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
